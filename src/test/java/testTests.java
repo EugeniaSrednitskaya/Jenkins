15dd208046bfef83d5674ddeb07fc59fc3bbf333
EugeniaSrednitskaya <103644577+EugeniaSrednitskaya@users.noreply.github.com>
2022-06-25 16:04:50 +0300

    test

---
+++ src/test/java/testTests.java
@@ -0,0 +1,45 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Test;
+
+public class testTests {
+
+    private static WebDriver driver;
+
+    @Test
+    public void testTests() {
+
+        //System.setProperty(chromeDriver, driverPath);
+        ChromeOptions chromeOptions = new ChromeOptions();
+        String options = "--disable-gpu;--no-sandbox;--disable-dev-shm-usage;--headless;--window-size=1920,1080";
+
+        if (options != null) {
+            for (String argument : options.split(";")) {
+                chromeOptions.addArguments(argument);
+            }
+        }
+
+        WebDriverManager.chromedriver().setup();
+
+        driver  = new ChromeDriver(chromeOptions);
+        driver.get("http://localhost:8080");
+        //driver.get("http://192.168.99.100:8080/");
+
+//        driver.findElement(By.name("j_username")).sendKeys("EugeniaSrednitskaya");
+//        driver.findElement(By.name("j_password")).sendKeys("ffeea7a4cd5941178f942304df1f8a1f");
+//        driver.findElement(By.name("Submit")).click();
+
+        Assert.assertTrue(driver.findElement(By.id("security-token")).isDisplayed());
+
+    }
+
+    @AfterTest
+    public void exit() {
+        driver.quit();
+    }
+}
