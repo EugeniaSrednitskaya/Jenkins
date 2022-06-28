d558427125636d194010c55a384e173aad984b48
EugeniaSrednitskaya <103644577+EugeniaSrednitskaya@users.noreply.github.com>
2022-06-28 19:39:38 +0300

    Test without Jenkins 1

--- src/test/java/runner/BaseTest.java
+++ src/test/java/runner/BaseTest.java
@@ -1,39 +1,15 @@
 package runner;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.testng.annotations.*;
-
-import static runner.BaseUtils.getProperty;
 
 public abstract class BaseTest {
 
     private WebDriver driver;
-    static final String PREFIX_PROP = "default.";
-    private static final String PROP_ADMIN_USERNAME = PREFIX_PROP + "admin.username";
-    //private static final String PROP_ADMIN_USERNAME = "default.admin.username";
-    private static final String PROP_ADMIN_PAS = PREFIX_PROP + "admin.password";
-    private static final String PROP_PORT = PREFIX_PROP + "port";
 
     @BeforeMethod
     protected void beforeMethod() {
-
         driver = BaseUtils.createDriver();
-        driver.get(String.format("http://localhost:%s", getProperty(PROP_PORT)));
-//        BaseUtils.get(driver);
-
-        WebElement name = driver.findElement(By.name("j_username"));
-        name.sendKeys(getProperty(PROP_ADMIN_USERNAME));
-        //name.sendKeys("EugeniaSrednitskaya");
-
-        WebElement password = driver.findElement(By.name("j_password"));
-        password.sendKeys(getProperty(PROP_ADMIN_PAS));
-        //password.sendKeys("ffeea7a4cd5941178f942304df1f8a1f");
-
-        WebElement SignIn = driver.findElement(By.name("Submit"));
-        SignIn.click();
-
     }
 
     @AfterMethod
--- src/test/java/runner/BaseUtils.java
+++ src/test/java/runner/BaseUtils.java
@@ -5,74 +5,39 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 
-
 public final class BaseUtils {
 
     private static final String CHROME_OPTIONS = "CHROME_OPTIONS";
     private static final String CHROME_OPTIONS_PROP = "default." + CHROME_OPTIONS.toLowerCase();
-    static final String PREFIX_PROP = "default.";
-    private static final String PROP_PORT = PREFIX_PROP + "port";
-    protected static FileInputStream fileInputStream;
+
     private static Properties properties;
 
-//    static Properties getProperties() {
-//        return properties;
-//    }
-
     private static void initProperties() {
-        try {
-            //указание пути до файла с настройками
-            fileInputStream = new FileInputStream("src/test/resources/config.properties");
+        if (properties == null) {
             properties = new Properties();
-            properties.load(fileInputStream);
-        } catch (IOException e) {
-            e.printStackTrace();
-            //обработка возможного исключения (нет файла и т.п.)
-        } finally {
-            if (fileInputStream != null)
+            if (isServerRun()) {
+                properties.setProperty(CHROME_OPTIONS_PROP, System.getenv(CHROME_OPTIONS));
+            } else {
                 try {
-                    fileInputStream.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                    InputStream inputStream = BaseUtils.class.getClassLoader().getResourceAsStream("config.properties");
+                    if (inputStream == null) {
+                        System.out.println("ERROR: The \u001B[31mconfig.properties\u001B[0m file not found in src/test/resources/ directory.");
+                        System.out.println("You need to create it from config.properties.TEMPLATE file.");
+                        System.exit(1);
+                    }
+                    properties.load(inputStream);
+                } catch (IOException ignore) {}
+            }
         }
     }
 
-    /**
-     * метод для возврата строки со значением из файла с настройками
-     */
-    public static String getProperty(String key) {
-        return properties.getProperty(key);
-    }
-
-//    private static void initProperties() {
-//        if (properties == null) {
-//            properties = new Properties();
-//            if (isServerRun()) {
-//                properties.setProperty(CHROME_OPTIONS_PROP, System.getenv(CHROME_OPTIONS));
-//            } else {
-//                try {
-//                    InputStream inputStream = BaseUtils.class.getClassLoader().getResourceAsStream("config.properties");
-//                    if (inputStream == null) {
-//                        System.out.println("ERROR: The \u001B[config.properties\u001B[0m file not found in src/test/resources/ directory.");
-//                        System.out.println("You need to create it from config.properties.TEMPLATE file.");
-//                        System.exit(1);
-//                    }
-//                    properties.load(inputStream);
-//                } catch (IOException ignore) {}
-//            }
-//        }
-//    }
-
 
     private static final ChromeOptions chromeOptions;
-
     static {
         initProperties();
 
@@ -97,9 +62,4 @@ public final class BaseUtils {
 
         return driver;
     }
-
-//    static void get(WebDriver driver) {
-//        driver.get(String.format("http://localhost:%s", getProperties().getProperty(PROP_PORT)));
-//        //driver.get("http://localhost:8080");
-//    }
 }
