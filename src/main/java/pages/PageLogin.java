8b6ccfa6dec2655b8f77eb36467ca0f5b0d13ec8
EugeniaSrednitskaya <103644577+EugeniaSrednitskaya@users.noreply.github.com>
2022-07-06 14:31:58 +0300

    PageBase&PageLogin

---
+++ src/main/java/pages/PageLogin.java
@@ -0,0 +1,23 @@
+package pages;
+
+import elements.ConfigurationProperties;
+import elements.LocatorsPageLogin;
+import org.openqa.selenium.WebDriver;
+
+public class PageLogin extends PageBase{
+
+    private LocatorsPageLogin element = new LocatorsPageLogin(getDriver());
+
+    public PageLogin(WebDriver existingDriver) {
+        super(existingDriver);
+    }
+
+    public void sendUser(String user) { element.getXpathLogin().sendKeys(user); }
+
+    public void sendPassword(String password) {
+        element.getXpathPassword().sendKeys(password);
+    }
+
+    public void clickSignIn() { element.getSubmitButton().click(); }
+
+}
