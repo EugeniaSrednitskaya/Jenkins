6ccaff97c791942196493e994320b71c5aea2088
EugeniaSrednitskaya <103644577+EugeniaSrednitskaya@users.noreply.github.com>
2022-07-06 11:37:22 +0300

    ConfigurationProperties

--- src/main/java/elements/ConfigurationProperties.java
+++ src/main/java/elements/ConfigurationProperties.java
@@ -1,12 +1,10 @@
 package elements;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
