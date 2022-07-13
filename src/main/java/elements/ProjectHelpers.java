5c21113844853a0ec86ec26c179a0870376d531c
EugeniaSrednitskaya <103644577+EugeniaSrednitskaya@users.noreply.github.com>
2022-07-13 17:22:30 +0300

    ProjectHelpers

--- src/main/java/elements/ProjectHelpers.java
+++ src/main/java/elements/ProjectHelpers.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.WebDriver;
 import pages.PageLogin;
 
 import static elements.ConfigurationProperties.PROPERTIES_PREFIX;
-import static java.lang.System.getProperties;
 
 public final class ProjectHelpers {
 
@@ -17,19 +16,23 @@ public final class ProjectHelpers {
 
     public static void getURL(WebDriver driver) {
         //driver.get(String.format("http://localhost:%s", getProperties().getProperty(PROPERTIES_PORT)));
-        driver.get(String.format(PROPERTIES_START_PAGE+"%s", getProperties().getProperty(PROPERTIES_PORT)));
+        driver.get("http://localhost:8080/");
+        //driver.get(String.format(PROPERTIES_START_PAGE+"%s", getProperties().getProperty(PROPERTIES_PORT)));
+        //driver.get(String.format(PROPERTIES_START_PAGE+"%s", ConfigurationProperties.getProperty(PROPERTIES_PORT)));
     }
 
     public static void login(WebDriver driver) {
         getURL(driver);
 
         PageLogin pageLogin = new PageLogin(driver);
-        pageLogin.sendUser(PROPERTIES_ADMIN_USERNAME);
-        pageLogin.sendPassword(PROPERTIES_ADMIN_PASSWORD);
+
+        pageLogin.sendUser(ConfigurationProperties.getProperty(PROPERTIES_ADMIN_USERNAME));
+        pageLogin.sendPassword(ConfigurationProperties.getProperty(PROPERTIES_ADMIN_PASSWORD));
         pageLogin.clickSignIn();
     }
 
     public static void logout(WebDriver driver) {
         driver.findElement(By.xpath("//a[@href='/logout']")).click();
     }
+
 }
