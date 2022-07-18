a886541b6fcc9c0824e9b7d77e8251e5a7e16fb3
EugeniaSrednitskaya <103644577+EugeniaSrednitskaya@users.noreply.github.com>
2022-07-18 12:45:21 +0300

    ConfigurationProperties

--- src/main/java/elements/ConfigurationProperties.java
+++ src/main/java/elements/ConfigurationProperties.java
@@ -75,6 +75,7 @@ public final class ConfigurationProperties {
     public WebDriver createDriver() {
         WebDriver driver = new ChromeDriver(chromeOptions);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
         return driver;
     }
 }
