9d2c8df7dae093b8b61c4c68bea54628cea50355
EugeniaSrednitskaya <103644577+EugeniaSrednitskaya@users.noreply.github.com>
2022-07-18 12:55:41 +0300

    PageMain

--- src/test/java/pages/PageMainTest.java
+++ src/test/java/pages/PageMainTest.java
@@ -5,11 +5,9 @@ import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import static elements.ConfigurationProperties.PROPERTIES_PREFIX;
+import static elements.ProjectHelpers.PROPERTIES_ADMIN_USERNAME;
 
 public class PageMainTest extends BeforeAfterTest{
-
-    private static final String PROPERTIES_ADMIN_USERNAME = PROPERTIES_PREFIX + "admin.username";
 
     private PageMain pageMain() {
         return new PageMain(getDriver());
@@ -28,20 +26,11 @@ public class PageMainTest extends BeforeAfterTest{
     
     @Test
     public void testClickSearch() {
-
         String textSearch = "Search item";
         pageMain().clickSearch(textSearch);
+
         Assert.assertTrue(getDriver()
                 .findElement(By.xpath("//div[@id = 'main-panel']/h1")).getText().contains(textSearch));
 
     }
-
-//    @Test
-//    public void testClickLogOut() {
-//
-//        pageMain().clickLogOut();
-//        Assert.assertTrue(getDriver()
-//                .findElement(By.xpath("//div[@id = 'loginIntroDefault']/h1"))
-//                .getText().equals("Welcome to Jenkins!"));
-//    }
 }
