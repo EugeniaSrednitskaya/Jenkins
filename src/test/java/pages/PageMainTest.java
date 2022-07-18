af66caada683ed653d1d860ae129ff19c9325874
EugeniaSrednitskaya <103644577+EugeniaSrednitskaya@users.noreply.github.com>
2022-07-18 15:58:16 +0300

    Update PageMainTest.java

--- src/test/java/pages/PageMainTest.java
+++ src/test/java/pages/PageMainTest.java
@@ -15,17 +15,20 @@ public class PageMainTest extends BeforeAfterTest{
         return new PageMain(getDriver());
     }
 
+    @Ignore
     @Test
     public void testHeaderDisplay() {
         Assert.assertTrue(pageMain().headerPageMainDisplayed());
     }
 
+    @Ignore
     @Test
     public void testUserNameCheck() {
         Assert.assertTrue(pageMain().userNameCheck().toLowerCase()
                 .contains(ConfigurationProperties.getProperty(PROPERTIES_ADMIN_USERNAME).toLowerCase()));
     }
     
+    @Ignore
     @Test
     public void testClickSearch() {
 
@@ -35,13 +38,4 @@ public class PageMainTest extends BeforeAfterTest{
                 .findElement(By.xpath("//div[@id = 'main-panel']/h1")).getText().contains(textSearch));
 
     }
-
-//    @Test
-//    public void testClickLogOut() {
-//
-//        pageMain().clickLogOut();
-//        Assert.assertTrue(getDriver()
-//                .findElement(By.xpath("//div[@id = 'loginIntroDefault']/h1"))
-//                .getText().equals("Welcome to Jenkins!"));
-//    }
 }
