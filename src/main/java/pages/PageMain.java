10f6973dcd95465f6ca3dc939c862a74b0a96945
EugeniaSrednitskaya <103644577+EugeniaSrednitskaya@users.noreply.github.com>
2022-07-21 17:52:33 +0300

    PageMain

--- src/main/java/pages/PageMain.java
+++ src/main/java/pages/PageMain.java
@@ -4,7 +4,9 @@ import elements.LocatorsPageMain;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 
-public class PageMain extends PageBase{
+import java.util.List;
+
+public class PageMain extends PageBase {
 
     private LocatorsPageMain element = new LocatorsPageMain(getDriver());
 
@@ -25,6 +27,15 @@ public class PageMain extends PageBase{
         element.getSearch().sendKeys(searchText, Keys.ENTER);
 
         return new PageSearch(getDriver());
+    }
+
+    public List<String> getSlidePanel() {
+        return List.of(element.getNewItem().getText(),
+                element.getPeople().getText(),
+                element.getBuildHistory().getText(),
+                element.getManageJenkins().getText(),
+                element.getMyViews().getText(),
+                element.getNewView().getText());
     }
 
     public void clickLogOut() {
---
+++ src/test/java/pages/PageMainTest.java
@@ -0,0 +1,47 @@
+package pages;
+
+import elements.ConfigurationProperties;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static elements.ProjectHelpers.PROPERTIES_ADMIN_USERNAME;
+
+public class PageMainTest extends BeforeAfterTest{
+
+    private PageMain pageMain() {
+        return new PageMain(getDriver());
+    }
+
+    @Test
+    public void testHeaderDisplay() {
+        Assert.assertTrue(pageMain().headerPageMainDisplayed());
+    }
+
+    @Test
+    public void testUserNameCheck() {
+        Assert.assertTrue(pageMain().userNameCheck().toLowerCase()
+                .contains(ConfigurationProperties.getProperty(PROPERTIES_ADMIN_USERNAME).toLowerCase()));
+    }
+
+    @Test
+    public void testClickSearch() {
+        String textSearch = "Search item";
+
+        Assert.assertTrue(pageMain().getSearchResult(textSearch).getTextSearch().contains(textSearch));
+    }
+
+    @Test
+    public void testCheckSlidePanel() {
+        List<String> expectedListMenuElements = List.of(
+                "New Item",
+                "People",
+                "Build History",
+                "Manage Jenkins",
+                "My Views",
+                "New View");
+
+        Assert.assertEquals(pageMain().getSlidePanel(), expectedListMenuElements);
+    }
+}
